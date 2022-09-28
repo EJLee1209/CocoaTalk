be36ca915602d6295e7da034d56f7c60eb2dfd13
EunJae <dldmswo1209@naver.com>
2022-09-28 19:09:22 +0900

    앱 ui 틀 제작

--- app/src/main/java/com/dldmswo1209/cocoatalk/MainActivity.kt
+++ app/src/main/java/com/dldmswo1209/cocoatalk/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dldmswo1209.cocoatalk
 import android.os.*
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.dldmswo1209.cocoatalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val friendFragment = FriendFragment()
+        val chatFragment = ChatFragment()
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        replaceFragment(friendFragment)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.friend ->{
+                    replaceFragment(friendFragment)
+                    binding.toolbarTextView.text = "친구"
+                }
+
+                R.id.chat -> {
+                    replaceFragment(chatFragment)
+                    binding.toolbarTextView.text = "채팅"
+                }
+            }
+            true
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFrameLayout, fragment)
+            .commit()
     }
 }
 
